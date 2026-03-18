f3cffcc712943803f906bb5f8fdad7631a1bcddf
Nardelli <pedronardelli2104@gmail.com>
2026-03-18 11:31:13 -0300

    feat: ajuste de cors tirando o pom

--- src/main/java/com/api/config/CorsConfig.java
+++ src/main/java/com/api/config/CorsConfig.java
@@ -1,19 +1,23 @@
 package com.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://app-videira-natural.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // O OPTIONS aqui já resolve o Preflight
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600); // Dica: mantém a permissão no navegador por 1 hora
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://app-videira-natural.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // Libera todos: OPTIONS, POST, etc.
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
