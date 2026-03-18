069d3b057aa4156205a55089680be3e60d328d71
Nardelli <pedronardelli2104@gmail.com>
2026-03-18 09:11:55 -0300

    feat: Liberado Cors

--- src/main/java/com/api/controller/AuthController.java
+++ src/main/java/com/api/controller/AuthController.java
@@ -1,18 +1,25 @@
 package com.api.controller;
 
+import com.api.dto.AuthenticationDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.OPTIONS)
-    public ResponseEntity<?> options() {
-        return ResponseEntity.ok().build();
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody AuthenticationDTO request) {
+        // Como está aberto para teste, você pode apenas retornar um OK
+        // e um token falso para o seu Front não travar
+        Map<String, String> response = new HashMap<>();
+        response.put("token", "token-de-teste-liberado");
+        response.put("usuario", request.login());
+
+        return ResponseEntity.ok(response);
     }
 }
